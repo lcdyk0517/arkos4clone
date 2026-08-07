388c1a93ac6c9702c573a7cb7a0097bca3d7ec02
lcdyk0517 <lcdyk0517@qq.com>
2026-08-07 16:15:10 +0800

    fix n64 start script

--- replace_file/n64.sh
+++ replace_file/n64.sh
@@ -62,7 +62,7 @@ elif [[ $1 == "standalone-Glide64mk2" ]]; then
     /opt/mupen64plus/mupen64plus --set Video-Glide64mk2[aspect]=1 --resolution "${xres}x${yres}" --plugindir /opt/mupen64plus --gfx mupen64plus-video-glide64mk2.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$game"
   else
     /opt/mupen64plus/mupen64plus --set Video-Glide64mk2[aspect]=-1 --resolution "${xres}x${yres}" --plugindir /opt/mupen64plus --gfx mupen64plus-video-glide64mk2.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$game"
-  f
+  fi
 elif [[ $1 == "standalone-GlideN64" ]]; then
   if [[ $2 == "Widescreen_Aspect" ]]; then
     /opt/mupen64plus/mupen64plus --set Video-GLideN64[ThreadedVideo]=True --set Video-GLideN64[UseNativeResolutionFactor]=1 --set Video-GLideN64[AspectRatio]=3 --resolution "${xres}x${yres}" --plugindir /opt/mupen64plus --gfx mupen64plus-video-GLideN64.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$game"
